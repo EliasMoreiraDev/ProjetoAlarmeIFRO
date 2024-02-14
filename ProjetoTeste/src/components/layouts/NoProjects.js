70c7b5c5d82441f3ad37c1beba6c3034307ff93c
Elias Moreira <elias.moreiradossantos.28@gmail.com>
2024-02-14 17:57:33 -0400

    eeas

--- ProjetoTeste/src/components/layouts/NoProjects.js
+++
@@ -1,9 +0,0 @@
-import styles from './NoProjects.module.css'
-
-function NoProjects(){
-    return (
-    <div className={styles.container_text}>
-        <h2>Não há Horarios cadastrados...</h2>
-    </div>)
-}
-export default NoProjects
